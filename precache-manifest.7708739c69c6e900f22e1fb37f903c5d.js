c6deb76dcdc60fc3d54656ad8891ee0bc5b1a79b
Anis Campos <yocorporation@hotmail.com>
2020-01-10 09:29:59 -0500

    Updates

---
+++ precache-manifest.7708739c69c6e900f22e1fb37f903c5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d8dcb434b4fb8fd0eb6f9c96cf85df",
+    "url": "/fcc-mardow-preview/index.html"
+  },
+  {
+    "revision": "b0cc27d2a2650c576530",
+    "url": "/fcc-mardow-preview/static/css/2.679228d6.chunk.css"
+  },
+  {
+    "revision": "b2e113741488eaa1ed63",
+    "url": "/fcc-mardow-preview/static/css/main.00adc7e5.chunk.css"
+  },
+  {
+    "revision": "b0cc27d2a2650c576530",
+    "url": "/fcc-mardow-preview/static/js/2.a99d7381.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/fcc-mardow-preview/static/js/2.a99d7381.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2e113741488eaa1ed63",
+    "url": "/fcc-mardow-preview/static/js/main.766fb6bd.chunk.js"
+  },
+  {
+    "revision": "b1627a5315f876223649",
+    "url": "/fcc-mardow-preview/static/js/runtime-main.f875569f.js"
+  },
+  {
+    "revision": "313b144a1598e657a413102cbee46018",
+    "url": "/fcc-mardow-preview/static/media/test.313b144a.md"
+  }
+]);
