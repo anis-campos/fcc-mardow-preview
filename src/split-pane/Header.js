6596ccc9453320fdbd266f790c118ea0fa7bade0
Anis DA SILVA CAMPOS <yocorporation@hotmail.com>
2020-01-23 14:56:43 -0500

    Using Box from material UI

--- src/split-pane/Header.js
+++ src/split-pane/Header.js
@@ -10,7 +10,7 @@ import {
     ViewCompact as Both,
     ViewDay as Hor
 } from '@material-ui/icons'
-import {Button, ButtonGroup} from '@material-ui/core'
+import {Box, Button, ButtonGroup} from '@material-ui/core'
 import {BOTH, EDITOR, PREVIEW, VERTICAL} from "./constants";
 
 export const Header = (props) => {
@@ -18,37 +18,45 @@ export const Header = (props) => {
     const DirectionIcon = props.direction === VERTICAL ? Ver : Hor;
     const title = props.title || "SplitPane Title";
     return (
-        <div className={"split-pane-header"}
-             css={css`
-                        text-align: left; 
-                        border-bottom: white solid 2px;
-                        width: 100%; 
-                        margin: 0;
-                        height: 10vh;
-                        padding: 10px 50px 0 50px;
-             `}>
-            <span>{title}</span>
+        <Box display="flex" alignItems="flex-start" className={"split-pane-header"}
+            // css={css`
+            //            text-align: left;
+            //            border-bottom: white solid 2px;
+            //            width: 100%;
+            //            margin: 0;
+            //            height: 10vh;
+            //            padding: 10px 50px 0 50px;
+            // `}
+        >
+
+            {/*<span css={css`*/}
+            {/*            flex: 5;*/}
+            {/* `}></span>*/}
+            <Box pl="50px">{title}</Box>
+
+            <Box alignSelf="flex-end">
+                <Box mr="10px"><Button marginRight="10px" color="primary" variant="contained"
+                                       css={css`margin-right: 10px`}
+                                       onClick={props.toggleDirection}><DirectionIcon/>
+                </Button></Box>
+                <ButtonGroup color="primary">
+                    <Button variant={props.editorState === BOTH ? "contained" : null}
+                            onClick={() => props.setEditorState(BOTH)}
+                            css={css``}> <Both/>
+                    </Button>
+                    <Button variant={props.editorState === EDITOR ? "contained" : null}
+                            onClick={() => props.setEditorState(EDITOR)}
+                            css={css``}> <EditorIcon/>
+                    </Button>
+                    <Button variant={props.editorState === PREVIEW ? "contained" : null}
+                            onClick={() => props.setEditorState(PREVIEW)}
+                            css={css``}> <PreviewIcon/>
+                    </Button>
+                </ButtonGroup>
+            </Box>
 
 
-            <Button color="primary" variant="contained" onClick={props.toggleDirection}><DirectionIcon/>
-            </Button>
-            <ButtonGroup color="primary">
-                <Button variant={props.editorState === BOTH ? "contained" : null}
-                        onClick={() => props.setEditorState(BOTH)}
-                        css={css``}> <Both/>
-                </Button>
-                <Button variant={props.editorState === EDITOR ? "contained" : null}
-                        onClick={() => props.setEditorState(EDITOR)}
-                        css={css``}> <EditorIcon/>
-                </Button>
-                <Button variant={props.editorState === PREVIEW ? "contained" : null}
-                        onClick={() => props.setEditorState(PREVIEW)}
-                        css={css``}> <PreviewIcon/>
-                </Button>
-            </ButtonGroup>
-
-
-        </div>
+        </Box>
     )
 };
 
